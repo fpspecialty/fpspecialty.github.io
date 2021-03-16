d592dbca38bda46346c9ff4b2e73a755f0b3b9cf
Vladimir Logachev <logachev.dev@ya.ru>
2021-03-16 21:44:05 +0700

    Upd

--- assets.config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Resize DIR MAX_WIDTH QUALITY 
-bash scripts/process_images.sh "./build/images/projects" "200" "80"
-bash scripts/process_images.sh "./build/images/events" "300" "80"
--- src/Session.elm
+++
@@ -1,9 +0,0 @@
-module Session exposing (..)
-
-import Browser.Navigation as Nav
-import Geo exposing (Geo)
-
-type alias Session =
-    { geo : Geo
-    , navKey : Nav.Key
-    }
